58f86df89adf8874b169b68f4a70443fdf6c7ed7
Itai Azaria <itaiaz@amazon.com>
2023-08-07 09:29:38 +0300

    Use cache for keys and add dummy refresh/subscribe buttons

--- rail_bot.py
+++ rail_bot.py
@@ -8,21 +8,29 @@ import dateutil
 import humanize
 from dateutil.parser import parse
 from telegram import Update, InlineKeyboardMarkup, \
-    InlineKeyboardButton, Bot
+    InlineKeyboardButton
 from telegram.ext import CallbackQueryHandler, ApplicationBuilder, CommandHandler, ContextTypes, ConversationHandler, \
-    CallbackContext
+    CallbackContext, CallbackDataCache, ExtBot
 
 from train_facade import get_train_times, get_delay_from_api
 from train_stations import TRAIN_STATIONS
 
+FROM_STATION_KEY = 'from_station'
+TO_STATION_KEY = 'to_station'
+SELECTED_STATION_KEY = 'selected_station'
+TIME_KEY = 'time'
+
 SELECTED_ID = 'si'
 NEXT_STATE = 'ns'
 
-SELECT_ARRIVAL_STATION = 'c_1'
-SELECT_DEPARTURE_TIME = 'c_2'
-CHECK_DELAYS_FOR_SPECIFIC_TIME = 'c_3'
+SELECT_ARRIVAL_STATION = 'c1'
+SELECT_DEPARTURE_TIME = 'c2'
+CHECK_DELAYS_FOR_SPECIFIC_TIME = 'c3'
+REFRESH = 'c4'
+SUBSCRIBE_TO_SPECIFIC = 'c5'
 
 bot = None
+callback_data_cache: CallbackDataCache | None = None
 
 
 def next_state_is(state):
@@ -41,9 +49,12 @@ async def bot_error_handler(update: Optional[object], context: CallbackContext):
 
 
 def start_bot(token):
-    global bot
-    bot = Bot(token)
-    application = ApplicationBuilder().token(token).build()
+    global bot, callback_data_cache
+    bot = ExtBot(token)
+    # await bot.initialize()
+    # callback_data_cache = CallbackDataCache(bot)
+    # callback_data_cache = bot.callback_data_cache
+    application = ApplicationBuilder().arbitrary_callback_data(True).token(token).build()
 
     application.add_error_handler(bot_error_handler)
 
@@ -73,12 +84,12 @@ async def select_departure_station(update: Update, context: ContextTypes.DEFAULT
     return "select_arrival_station"
 
 
-def get_station_buttons(stations, next_state):
+def get_station_buttons(stations, next_state, additional_context={}):
     stations = sorted(stations, key=lambda station: station['english'])
     return [[InlineKeyboardButton(station['english'], callback_data=json.dumps({
-        NEXT_STATE: next_state,
-        SELECTED_ID: station['id']
-    }))]
+                                                                                   NEXT_STATE: next_state,
+                                                                                   SELECTED_STATION_KEY: station['id']
+                                                                               } | additional_context))]
             for station in stations]
 
 
@@ -87,15 +98,17 @@ async def select_arrival_station(update: Update, context: ContextTypes.DEFAULT_T
                  context.chat_data)
 
     callback_data = json.loads(update.callback_query.data)
-    departure_station_id = callback_data['si']
-    context.chat_data['departure_station_id'] = departure_station_id
+    departure_station_id = callback_data[SELECTED_STATION_KEY]
+    # context.chat_data['departure_station_id'] = departure_station_id
 
     # Get the user's selected departure stations
+    additional_context = {FROM_STATION_KEY: departure_station_id}
     buttons = get_station_buttons(list(filter(lambda station: station['id'] != departure_station_id, TRAIN_STATIONS)),
-                                  SELECT_DEPARTURE_TIME)
+                                  SELECT_DEPARTURE_TIME, additional_context)
 
     reply_markup = InlineKeyboardMarkup(buttons)
-    departure_station_name = next(filter(lambda s: s['id'] == departure_station_id, TRAIN_STATIONS))['english'].replace('-', '\-')
+    departure_station_name = next(filter(lambda s: s['id'] == departure_station_id, TRAIN_STATIONS))['english'].replace(
+        '-', '\-')
 
     await update.callback_query.message.edit_text(
         'Departing from *{departure_station}*\. Select arrival station:'.format(
@@ -135,17 +148,24 @@ def fmt_interval(dep, arr):
 
 
 async def select_departure_time(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logging.info("Got select_arrival_station command from user=%s, context=%s, chat_id=%s", update.effective_user,
+    logging.info("Got select_departure_time command from user=%s, context=%s, chat_id=%s", update.effective_user,
                  context.chat_data, update.effective_chat.id)
 
     query = update.callback_query
+    # callback_data_cache.process_callback_query(query)
     callback_data = json.loads(update.callback_query.data)
-    selected_station_id = callback_data['si']
-    context.chat_data['arrival_station_id'] = selected_station_id
-    departure_station_id = context.chat_data['departure_station_id']
 
-    all_times = get_train_times(context.chat_data['departure_station_id'],
-                                context.chat_data['arrival_station_id'])
+    logging.info("Callback data %s", callback_data)
+
+    to_station_id = callback_data[SELECTED_STATION_KEY]
+    # context.chat_data['arrival_station_id'] = to_station_id
+    # from_station_id = context.chat_data['from_station_id']
+    from_station_id = callback_data[FROM_STATION_KEY]
+
+    additional_context = {FROM_STATION_KEY: from_station_id, TO_STATION_KEY: to_station_id}
+
+    all_times = get_train_times(from_station_id,
+                                to_station_id)
     now = datetime.now()
 
     def is_applicable_time(time):
@@ -167,12 +187,17 @@ async def select_departure_time(update: Update, context: ContextTypes.DEFAULT_TY
         InlineKeyboardButton(
             "{dep} ({duration})".format(dep=format_time_from_str(dep, show_day=False),
                                         duration=fmt_interval(dep, arr)).capitalize(),
-            callback_data=json.dumps({'ns': CHECK_DELAYS_FOR_SPECIFIC_TIME, 'time': dep}))
+            callback_data=json.dumps({'ns': CHECK_DELAYS_FOR_SPECIFIC_TIME, TIME_KEY: dep} | additional_context))
         for dep, arr in times] for times in time_by_hour]
 
     reply_markup = InlineKeyboardMarkup(buttons)
-    departure_station_name = next(filter(lambda s: s['id'] == departure_station_id, TRAIN_STATIONS))['english'].replace('-', '\-')
-    arrival_station_name = next(filter(lambda s: s['id'] == selected_station_id, TRAIN_STATIONS))['english'].replace('-', '\-')
+    # reply_markup = callback_data_cache.process_keyboard(reply_markup)
+
+
+    departure_station_name = next(filter(lambda s: s['id'] == from_station_id, TRAIN_STATIONS))['english'].replace(
+        '-', '\-')
+    arrival_station_name = next(filter(lambda s: s['id'] == to_station_id, TRAIN_STATIONS))['english'].replace(
+        '-', '\-')
 
     await query.message.edit_text(
         "Departing from *{departure_station}* to *{arrival_station}*\. Please select departure time:".format(
@@ -187,20 +212,33 @@ async def check_delays_for_specific_time(update: Update, context: ContextTypes.D
                  context.chat_data)
 
     query = update.callback_query
+    # callback_data_cache.process_callback_query(callback_query=query)
     callback_data = json.loads(query.data)
+    logging.info("Callback data %s", callback_data)
+
     selected_time = callback_data['time']
-    departure_station_id = context.chat_data['departure_station_id']
-    arrival_station_id = context.chat_data['arrival_station_id']
+    departure_station_id = callback_data[FROM_STATION_KEY]
+    arrival_station_id = callback_data[TO_STATION_KEY]
 
     logging.info("Checking delay from %s to %s on %s", departure_station_id, arrival_station_id, selected_time)
 
     train_times = get_delay_from_api(departure_station_id,
                                      arrival_station_id, selected_time)
 
+    reply_markup = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("Refresh", callback_data=json.dumps(
+            {NEXT_STATE: REFRESH, FROM_STATION_KEY: departure_station_id, TO_STATION_KEY: arrival_station_id,
+             TIME_KEY: selected_time})),
+          InlineKeyboardButton("Subscribe", callback_data=json.dumps(
+              {NEXT_STATE: SUBSCRIBE_TO_SPECIFIC, FROM_STATION_KEY: departure_station_id,
+               TO_STATION_KEY: arrival_station_id, TIME_KEY: selected_time}))]])
+
+    # reply_markup = callback_data_cache.process_keyboard(reply_markup)
+
     response_txt = format_delay_response(train_times, selected_time, departure_station_id, arrival_station_id)
 
     if to_user is None:
-        await query.message.edit_text(response_txt, parse_mode='MarkdownV2')
+        await query.message.edit_text(response_txt, parse_mode='MarkdownV2', reply_markup=reply_markup)
     else:
         pass
 
@@ -208,13 +246,15 @@ async def check_delays_for_specific_time(update: Update, context: ContextTypes.D
 
 
 def format_delay_response(train_times, selected_time, departure_station_id, arrival_station_id):
-    departure_station_name = next(filter(lambda s: s['id'] == departure_station_id, TRAIN_STATIONS))['english'].replace('-', '\-')
-    arrival_station_name = next(filter(lambda s: s['id'] == arrival_station_id, TRAIN_STATIONS))['english'].replace('-', '\-')
+    departure_station_name = next(filter(lambda s: s['id'] == departure_station_id, TRAIN_STATIONS))['english'].replace(
+        '-', '\-')
+    arrival_station_name = next(filter(lambda s: s['id'] == arrival_station_id, TRAIN_STATIONS))['english'].replace('-',
+                                                                                                                    '\-')
     last_update_str = '(updated {})'.format(datetime.strftime(datetime.now(), '%H:%M'))
     if train_times.delay_in_minutes > 0:
         # delay_str = "is ⏱️ {delay} minutes late".format(delay=str(train_times))
         departure_str = '️️~{}~ ⏱ {}'.format(format_time_from_str(train_times.original_departure),
-                                               format_time(train_times.get_updated_departure(), False))
+                                             format_time(train_times.get_updated_departure(), False))
         arrival_str = '{}'.format(format_time(train_times.get_updated_arrival(), False))
     else:
         departure_str = '✅ {}'.format(format_time(train_times.get_updated_departure()))
